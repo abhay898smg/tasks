4d86d1cbf04d6d8c723d9c44ecbe69c24d36daab
abhay898smg <abhaysunka898@gmail.com>
2025-04-27 10:31:51 +0530

    Add files via upload

---
+++ basicmathsop.py
@@ -0,0 +1,12 @@
+x=input('x=')
+y=input('y:')
+x=float(x)
+y=float(y)
+a=x+y
+b=x-y
+c=x*y
+d=x/y
+print("sum is :",a)
+print("difference is:",b)
+print("product is :",c)
+print("quotient is :",d)
---
+++ greet.py
@@ -0,0 +1,5 @@
+a=input('enter your first name:')
+b=input('enter your last name:')
+c= a + b
+print("hello," + c + "! welcome to python world")
+
